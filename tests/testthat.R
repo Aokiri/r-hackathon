443f2b372b85d5a5bbed49182303a3ecb34b7237
Alan D. <alandor95@hotmail.com>
2026-06-18 14:37:30 +0200

    Init: Added the entire folder layout

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(hackpkg)
+test_check("hackpkg")
---
+++ tests/testthat/test-add_numbers.R
@@ -0,0 +1,4 @@
+test_that("add_numbers works", {
+  expect_equal(add_numbers(1, 2), 3)
+  expect_equal(add_numbers(-1, 1), 0)
+})

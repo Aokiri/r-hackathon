495230c044bd37cacf3e1209a698ad46d2a7c1b3
Alan D. <alandor95@hotmail.com>
2026-06-19 11:10:06 +0200

    fix example_workflow.R paths to use system.file()

--- inst/example_workflow.R
+++ inst/example_workflow.R
@@ -4,7 +4,7 @@ REGION <- "Romandie / Valais"
 DATE   <- "2026-06-19"
 TIMES  <- c("08:00", "10:00", "12:00", "14:00", "16:00")
 
-stations    <- read_stations("data/SwissCities.csv", REGION)
+stations    <- read_stations(system.file("extdata", "SwissCities.csv", package = "hackpkg"), REGION)
 query_table <- build_query_table(stations, DATE, TIMES)
 
 all_routes <- dplyr::bind_rows(
@@ -19,6 +19,6 @@ all_routes <- dplyr::bind_rows(
 
 waiting <- compute_waiting_times(all_routes, query_table)
 
-map <- waiting_time_map(stations, waiting, "data/boundaries")
+map <- waiting_time_map(stations, waiting, system.file("extdata", "boundaries", package = "hackpkg"))
 print(map)
 ggplot2::ggsave("waiting_time_map.png", map, width = 10, height = 7)
